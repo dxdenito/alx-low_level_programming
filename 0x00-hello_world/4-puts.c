c6a198e76de3d8c8004c6cd7e0c45b4ccaf05c3b
Denis Karanja <denixkaranjah@gmail.com>
2022-09-08 03:54:10 -0700

    hello worl c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* begining of main program */
+
+int main(void)
+{
+	/* use char to input text and puts to display */
+
+	char hello[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(hello);
+
+	return (0);
+}
