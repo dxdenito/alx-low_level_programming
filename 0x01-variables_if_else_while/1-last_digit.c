4f008290108958b386e966da1f6532e02ba1f169
Denis Karanja <denixkaranjah@gmail.com>
2022-09-09 03:13:15 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - main program
+ * Return: 0
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+	int m;
+	char f[] = "Last digit of";
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+
+	m = n % 10;
+	if (m > 5)
+	{
+		printf("%s %d is %d and is greater than 5\n", f, n, m);
+	}
+
+	if (m == 0)
+	{
+		printf("%s %d is %d and is 0\n", f, n, m);
+	}
+
+	if ((m < 6) && (m != 0))
+	{
+		printf("%s %d is %d and is less than 6 and not 0", f, n, m);
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -main program
+ * Return: 0
+ */
+
+int main(void)
+{
+	char a_1;
+
+	for (a_1 = 'a'; a_1 <= 'z'; a_1++)
+	{
+		putchar(a_1);
+		putchar('\n');
+	}
+
+	return (0);
+}
