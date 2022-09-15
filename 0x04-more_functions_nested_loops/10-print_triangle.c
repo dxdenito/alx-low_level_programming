fe7f20c55856ee122a1fdab11653909b8ad88cce
dxdenito <denixkaranjah@gmail.com>
2022-09-15 19:21:40 +0300

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -2,7 +2,7 @@
 
 /**
  * print_triangle - Print triangle dependent on the parameter size.
- * @size : The height of the triangle
+ * @size: The height of the triangle
  * Return: Void.
  */
 void print_triangle(int size)
