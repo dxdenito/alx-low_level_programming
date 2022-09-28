8dd07896d17674b130c9b09e99cdce6c426c24e5
Denis Karanja <denixkaranjah@gmail.com>
2022-09-28 04:52:24 -0700

    main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,4 +7,18 @@ int  _putchar(char c);
 
 void _print_rev_recursion(char *s);
 
+int _strlen_recursion(char *s);
+
+int factorial(int n);
+
+int _pow_recursion(int x, int y);
+
+int _sqrt_recursion(int n);
+
+int sqrt(int c, int i);
+
+int is_prime_number(int n);
+
+int is_div(int num, int div)
+
 #endif /* MAIN_H */
