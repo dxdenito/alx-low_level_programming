4b346afa671181bda54d0b38fb8ef4714d5796fd
Denis Karanja <denixkaranjah@gmail.com>
2022-09-30 08:05:57 -0700

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program to add 2 numbers
+ * @argc: argument count
+ * @argv: arguement variables
+ * return 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum;
+
+	sum = 0;
+
+	for (i = 0; i < argc; i++)
+	{
+		if (i > 0)
+		{
+			if (atoi(argv[i]) == 0)
+			{
+				return (printf("Error\n"), 1);
+			}
+			sum += atoi(argv[i]);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
