93373f05275df0098f4507845e7b9fcf7d03658e
Denis Karanja <denixkaranjah@gmail.com>
2022-09-30 04:33:47 -0700

    you better work

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * *_strcat - function to concatenate
+ * @dest: first string
+ * @src: second string
+ * Return: char
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
