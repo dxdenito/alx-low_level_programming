48b4f189cfe4e0fd2ff5bf26f4ea7da123eae95f
Denis Karanja <denixkaranjah@gmail.com>
2022-10-03 23:53:17 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strdup -function to return pointer to a newly created space
+ * @str: string to be saved in space
+ * Return: null if null and pointer on success
+ */
+
+char *_strdup(char *str)
+{
+	char *a;
+	int i, c;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	a = malloc(i * sizeof(*a) + 1);
+	if (a == NULL)
+		return (NULL);
+
+	for (c = 0; c < i; c++)
+		a[c] = str[c];
+	a[c] = '\0';
+
+	return (a);
+}
