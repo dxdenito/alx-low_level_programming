3fc3597826c021f3ae3d72e5a2d6754e0d8f9ec0
Denis Karanja <denixkaranjah@gmail.com>
2022-10-11 03:20:00 -0700

    dog.h, readme filre

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - to show attributes of dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: this is a structure to be a ble to store attributes of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
